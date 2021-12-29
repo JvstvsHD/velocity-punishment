fe283fbc979fab454fc887bbe02265678ab4e706
JvstvsHD <fanoe@mailbox.org>
2021-12-29 16:51:52 +0100

    build.gradle.kts Updated velocity to 3.1.1 (hence the paper repo was added), jackson to 2.13.1 and junit to 5.8.2 The protocolize api has been removed, including its repo.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,26 +6,23 @@ group = "de.jvstvshd.punishment"
 version = "1.0.0-beta"
 
 repositories {
-    maven(url = "https://nexus.velocitypowered.com/repository/maven-public/")
-    maven(url = "https://mvn.exceptionflug.de/repository/exceptionflug-public/")
+    maven("https://nexus.velocitypowered.com/repository/maven-public/")
+    maven("https://papermc.io/repo/repository/maven-public/")
     mavenCentral()
 }
 
 dependencies {
-    compileOnly("com.velocitypowered", "velocity-api", "3.0.1")
-    compileOnly("com.velocitypowered", "velocity-proxy", "3.0.1")
-    compileOnly("io.netty:netty-handler:4.1.69.Final")
-    annotationProcessor("com.velocitypowered", "velocity-api", "3.0.1")
+    compileOnly("com.velocitypowered:velocity-api:3.1.1")
+    annotationProcessor("com.velocitypowered:velocity-api:3.1.1")
     compileOnly("net.luckperms:api:5.3")
 
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
 
     implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
     implementation("com.zaxxer:HikariCP:5.0.0")
-    compileOnly("dev.simplix:protocolize-api:2.0.0")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks.getByName<Test>("test") {
@@ -40,6 +37,5 @@ tasks {
     jar {
         duplicatesStrategy = DuplicatesStrategy.INCLUDE
         from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-        destinationDirectory.set(File("C:\\Server\\TestNetzwerk\\Proxy\\plugins"))
     }
 }
