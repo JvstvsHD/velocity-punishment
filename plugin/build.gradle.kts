aabf738d4c5a426da5ee8314e31986bc14309151
JvstvsHD <fanoe@mailbox.org>
2022-05-29 15:57:00 +0200

    Downgrade mariadb If using MariaDB Java Connector 3.x with hikari, an exception regarding no existing property setters is thrown.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation(project(":api"))
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
 
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.4")
+    implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
     implementation("com.zaxxer:HikariCP:5.0.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
